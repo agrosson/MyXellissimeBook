fffa38d6b482d6c5a275f0a67cf738f2bba57ee9
agrosson <agrosson@gmail.com>
2019-06-19 09:55:08 +0200

    Documentation for Book.swift

--- MyXellissimeBook/Model/Book.swift
+++ MyXellissimeBook/Model/Book.swift
@@ -11,11 +11,17 @@ import Firebase
 
 
 class Book: NSObject {
+    /// Unique Id of the book = give by ChildByAutoId from Firebase
     var uniqueId: String?
+    /// Title of the book
     var title: String?
+    /// Author(s) of the book
     var author: String?
+    /// Publisher of the book
     var editor: String?
+    /// Book Isbn 13 or other
     var isbn: String?
+    /// Boolean that tracks if book is available for loan or not
     var isAvailable: Bool?
 }
 
