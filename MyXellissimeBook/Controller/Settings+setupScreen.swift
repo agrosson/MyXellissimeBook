57397d2c6fcc168b389546612178ce97add51324
agrosson <agrosson@gmail.com>
2019-11-12 16:53:57 +0100

    Refactor massive settings view controller

---
+++ MyXellissimeBook/Controller/Settings+setupScreen.swift
@@ -0,0 +1,117 @@
+//
+//  Settings+setupScreen.swift
+//  MyXellissimeBook
+//
+//  Created by ALEXANDRE GROSSON on 12/11/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import GoogleMobileAds
+
+extension SettingsViewController {
+    /**
+     Function that sets up views on screen
+     */
+    func setupViews() {
+        view.addSubview(containerView)
+        containerView.addSubviews(modifyUserProfileImageButton,modifyUserPasswordButton,modifyUserNameButton,modifyUserEmailButton,tutorialButton)
+        containerView.backgroundColor? = mainBackgroundColor
+        setupModifyUserProfileImageButton()
+        setupModifyUserPasswordButton()
+        setupModifyUserNameButton()
+        setupModifyUserEmailButton()
+        setupTutorialButton()
+        NSLayoutConstraint.activate([
+            // ContainerView
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            // modifyUserProfileImageButton
+            modifyUserProfileImageButton.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 25 + topbarHeight),
+            modifyUserProfileImageButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            modifyUserProfileImageButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            modifyUserProfileImageButton.heightAnchor.constraint(equalToConstant: 60),
+            // modifyUserPasswordButton
+            modifyUserPasswordButton.topAnchor.constraint(equalTo: modifyUserProfileImageButton.bottomAnchor, constant: 25),
+            modifyUserPasswordButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            modifyUserPasswordButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            modifyUserPasswordButton.heightAnchor.constraint(equalToConstant: 60),
+            // modifyUserNameButton
+            modifyUserNameButton.topAnchor.constraint(equalTo: modifyUserPasswordButton.bottomAnchor, constant: 25),
+            modifyUserNameButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            modifyUserNameButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            modifyUserNameButton.heightAnchor.constraint(equalToConstant: 60),
+            // modifyUserEmailButton
+            modifyUserEmailButton.topAnchor.constraint(equalTo: modifyUserNameButton.bottomAnchor, constant: 25),
+            modifyUserEmailButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            modifyUserEmailButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            modifyUserEmailButton.heightAnchor.constraint(equalToConstant: 60),
+            // modifyUserEmailButton
+            tutorialButton.topAnchor.constraint(equalTo: modifyUserEmailButton.bottomAnchor, constant: 25),
+            tutorialButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            tutorialButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            tutorialButton.heightAnchor.constraint(equalToConstant: 60),
+            
+        ])
+    }
+    /**
+     Function that sets up modifyUserProfileImageButton
+     */
+   func setupTutorialButton() {
+        tutorialButton.setTitle("Lancer le tutorial", for: .normal)
+        tutorialButton.layer.cornerRadius = 15
+        tutorialButton.titleLabel?.font = .systemFont(ofSize: 25)
+        tutorialButton.addTarget(self, action: #selector(handleTutorial), for: .touchUpInside)
+    }
+    /**
+     Function that sets up NavigationBar
+     */
+    func setupNavigationBar(){
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Retour", style: .plain, target: self, action: #selector(handelCancel))
+        navigationItem.leftBarButtonItem?.tintColor = navigationItemColor
+        let textAttributes = [NSAttributedString.Key.foregroundColor:navigationItemColor]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        navigationItem.title = "Paramètres"
+    }
+    /**
+     Function that sets up modifyUserProfileImageButton
+     */
+    func setupModifyUserProfileImageButton() {
+        modifyUserProfileImageButton.setTitle("Changer ma photo", for: .normal)
+        modifyUserProfileImageButton.layer.cornerRadius = 15
+        modifyUserProfileImageButton.titleLabel?.font = .systemFont(ofSize: 25)
+        modifyUserProfileImageButton.addTarget(self, action: #selector(handleModifyProfilePhoto), for: .touchUpInside)
+    }
+    
+    /**
+     Function that sets up modifyUserPasswordButton
+     */
+    func setupModifyUserPasswordButton() {
+        modifyUserPasswordButton.setTitle("Changer mon mot de passe", for: .normal)
+        modifyUserPasswordButton.layer.cornerRadius = 15
+        modifyUserPasswordButton.titleLabel?.font = .systemFont(ofSize: 25)
+        modifyUserPasswordButton.addTarget(self, action: #selector(handleModifyPassword), for: .touchUpInside)
+    }
+    /**
+     Function that sets up modifyUserNameButton
+     */
+    func setupModifyUserNameButton() {
+        modifyUserNameButton.setTitle("Changer mon nom d'utilisateur", for: .normal)
+        modifyUserNameButton.layer.cornerRadius = 15
+        modifyUserNameButton.titleLabel?.font = .systemFont(ofSize: 25)
+        modifyUserNameButton.addTarget(self, action: #selector(handleModifyUserName), for: .touchUpInside)
+    }
+    /**
+     Function that sets up modifyUserEmailButton
+     */
+    func setupModifyUserEmailButton() {
+        modifyUserEmailButton.setTitle("Changer mon email", for: .normal)
+        modifyUserEmailButton.layer.cornerRadius = 15
+        modifyUserEmailButton.titleLabel?.font = .systemFont(ofSize: 25)
+        modifyUserEmailButton.addTarget(self, action: #selector(handleModifyEmail), for: .touchUpInside)
+    }
+}
--- MyXellissimeBook/Controller/Settings.swift
+++ MyXellissimeBook/Controller/Settings.swift
@@ -15,241 +15,77 @@ import GoogleMobileAds
 /**
  This class defines the InitialViewController
  */
-class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADRewardBasedVideoAdDelegate {
-   
-    
-    func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
-        print("here is the reward")
-    }
-   
+class SettingsViewController: UIViewController {
     let containerView = CustomUI().view
     let modifyUserProfileImageButton = CustomUI().button
     let modifyUserPasswordButton = CustomUI().button
     let modifyUserNameButton = CustomUI().button
     let modifyUserEmailButton = CustomUI().button
     let tutorialButton = CustomUI().button
-    
     /// User Profile Image
     var userProfileImage = UIImage()
     var email: String?
     
     /// Var to track if user is authorized to see the tutorial : 0 is no
     var userCanWatchTutorial = 0
-    
-    
     override func viewDidLoad() {
         email = Auth.auth().currentUser?.email
         setupNavigationBar()
         setupViews()
-        
-        // Video reward
-        GADRewardBasedVideoAd.sharedInstance().delegate = self
-       // GADRewardBasedVideoAd.sharedInstance().load(GADRequest(),withAdUnitID: valueForAPIKey(named: "GADRewardBasedVideoAd"))
-        // test
-        //GADRewardBasedVideoAd.sharedInstance().load(GADRequest(),withAdUnitID: valueForAPIKey(named: "testGADRewardBasedVideoAd"))
     }
     override func viewWillAppear(_ animated: Bool) {
         super .viewWillAppear(animated)
-        GADRewardBasedVideoAd.sharedInstance().load(GADRequest(),withAdUnitID: valueForAPIKey(named: "testGADRewardBasedVideoAd"))
     }
     
     typealias Completion = (Error?) -> Void
-    /**
-     Function that sets up views on screen
-     */
-    fileprivate func setupViews() {
-        view.addSubview(containerView)
-        containerView.addSubviews(modifyUserProfileImageButton,modifyUserPasswordButton,modifyUserNameButton,modifyUserEmailButton,tutorialButton)
-        containerView.backgroundColor? = mainBackgroundColor
-        setupModifyUserProfileImageButton()
-        setupModifyUserPasswordButton()
-        setupModifyUserNameButton()
-        setupModifyUserEmailButton()
-        setupTutorialButton()
-        NSLayoutConstraint.activate([
-            // ContainerView
-            containerView.topAnchor.constraint(equalTo: view.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            // modifyUserProfileImageButton
-            modifyUserProfileImageButton.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 25 + topbarHeight),
-            modifyUserProfileImageButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            modifyUserProfileImageButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            modifyUserProfileImageButton.heightAnchor.constraint(equalToConstant: 60),
-            // modifyUserPasswordButton
-            modifyUserPasswordButton.topAnchor.constraint(equalTo: modifyUserProfileImageButton.bottomAnchor, constant: 25),
-            modifyUserPasswordButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            modifyUserPasswordButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            modifyUserPasswordButton.heightAnchor.constraint(equalToConstant: 60),
-            // modifyUserNameButton
-            modifyUserNameButton.topAnchor.constraint(equalTo: modifyUserPasswordButton.bottomAnchor, constant: 25),
-            modifyUserNameButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            modifyUserNameButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            modifyUserNameButton.heightAnchor.constraint(equalToConstant: 60),
-            // modifyUserEmailButton
-            modifyUserEmailButton.topAnchor.constraint(equalTo: modifyUserNameButton.bottomAnchor, constant: 25),
-            modifyUserEmailButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            modifyUserEmailButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            modifyUserEmailButton.heightAnchor.constraint(equalToConstant: 60),
-            // modifyUserEmailButton
-            tutorialButton.topAnchor.constraint(equalTo: modifyUserEmailButton.bottomAnchor, constant: 25),
-            tutorialButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            tutorialButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            tutorialButton.heightAnchor.constraint(equalToConstant: 60),
-            
-        ])
-    }
-    /**
-     Function that sets up modifyUserProfileImageButton
-     */
-    fileprivate func setupTutorialButton() {
-        tutorialButton.setTitle("Lancer le tutorial", for: .normal)
-        tutorialButton.layer.cornerRadius = 15
-        tutorialButton.titleLabel?.font = .systemFont(ofSize: 25)
-        tutorialButton.addTarget(self, action: #selector(handleTutorial), for: .touchUpInside)
-    }
-    /**
-     Function that sets up NavigationBar
-     */
-    private func setupNavigationBar(){
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Retour", style: .plain, target: self, action: #selector(handelCancel))
-        navigationItem.leftBarButtonItem?.tintColor = navigationItemColor
-        let textAttributes = [NSAttributedString.Key.foregroundColor:navigationItemColor]
-        navigationController?.navigationBar.titleTextAttributes = textAttributes
-        navigationItem.title = "Paramètres"
-    }
-    /**
-     Function that sets up modifyUserProfileImageButton
-     */
-    fileprivate func setupModifyUserProfileImageButton() {
-        modifyUserProfileImageButton.setTitle("Changer ma photo", for: .normal)
-        modifyUserProfileImageButton.layer.cornerRadius = 15
-        modifyUserProfileImageButton.titleLabel?.font = .systemFont(ofSize: 25)
-        modifyUserProfileImageButton.addTarget(self, action: #selector(handleModifyProfilePhoto), for: .touchUpInside)
-    }
     
-    /**
-     Function that sets up modifyUserPasswordButton
-     */
-    fileprivate func setupModifyUserPasswordButton() {
-        modifyUserPasswordButton.setTitle("Changer mon mot de passe", for: .normal)
-        modifyUserPasswordButton.layer.cornerRadius = 15
-        modifyUserPasswordButton.titleLabel?.font = .systemFont(ofSize: 25)
-        modifyUserPasswordButton.addTarget(self, action: #selector(handleModifyPassword), for: .touchUpInside)
-    }
-    /**
-     Function that sets up modifyUserNameButton
-     */
-    fileprivate func setupModifyUserNameButton() {
-        modifyUserNameButton.setTitle("Changer mon nom d'utilisateur", for: .normal)
-        modifyUserNameButton.layer.cornerRadius = 15
-        modifyUserNameButton.titleLabel?.font = .systemFont(ofSize: 25)
-        modifyUserNameButton.addTarget(self, action: #selector(handleModifyUserName), for: .touchUpInside)
-    }
-    /**
-     Function that sets up modifyUserEmailButton
-     */
-    fileprivate func setupModifyUserEmailButton() {
-        modifyUserEmailButton.setTitle("Changer mon email", for: .normal)
-        modifyUserEmailButton.layer.cornerRadius = 15
-        modifyUserEmailButton.titleLabel?.font = .systemFont(ofSize: 25)
-        modifyUserEmailButton.addTarget(self, action: #selector(handleModifyEmail), for: .touchUpInside)
-    }
     /**
      Action that dismisses VC when "Retour" button clicked
      */
-    @objc private func handelCancel(){
+    @objc func handelCancel(){
         self.dismiss(animated: true, completion: nil)
     }
     /**
      Action that modifies user profile photo
      */
-    @objc private func handleModifyProfilePhoto(){
-        print("here modify photo")
+    @objc func handleModifyProfilePhoto(){
         // Display picker to get the new photo/Image
         launchPicker()
-        
     }
     fileprivate func launchTutorial() {
-        print("the tutorial is sent")
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let swipingController = SwipingController(collectionViewLayout: layout)
         swipingController.modalPresentationStyle = .fullScreen
         present(swipingController, animated: true, completion: nil)
     }
-    
     /**
      Action that modifies user profile photo
      */
-    @objc private func handleTutorial(){
-        if GADRewardBasedVideoAd.sharedInstance().isReady == true {
-          GADRewardBasedVideoAd.sharedInstance().present(fromRootViewController: self)
-        }
-        
+    @objc func handleTutorial(){
         launchTutorial()
     }
-    
-    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
-        didRewardUserWith reward: GADAdReward) {
-      print("Reward received with currency: \(reward.type), amount \(reward.amount).")
-    }
-
-    func rewardBasedVideoAdDidReceive(_ rewardBasedVideoAd:GADRewardBasedVideoAd) {
-      print("Reward based video ad is received.")
-    }
-
-    func rewardBasedVideoAdDidOpen(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-      print("Opened reward based video ad.")
-    }
-
-    func rewardBasedVideoAdDidStartPlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-      print("Reward based video ad started playing.")
-    }
-
-    func rewardBasedVideoAdDidCompletePlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-      print("Reward based video ad has completed.")
-    }
-
-    func rewardBasedVideoAdDidClose(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-      print("Reward based video ad is closed.")
-        // Reload an new videao in the background
-       GADRewardBasedVideoAd.sharedInstance().load(GADRequest(),withAdUnitID: valueForAPIKey(named: "GADRewardBasedVideoAd"))
-    }
-
-    func rewardBasedVideoAdWillLeaveApplication(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-      print("Reward based video ad will leave application.")
-    }
-
-    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
-        didFailToLoadWithError error: Error) {
-      print("Reward based video ad failed to load.")
-    // Reload an new videao in the background
-    }
-    
     /**
      Action that modifies user password
      */
-    @objc private func handleModifyPassword(){
+    @objc func handleModifyPassword(){
         print("here modify password")
         alertToModifyPassword(title: "Modification du mot de passe", message: "Indiquer l'ancien mot de passe et\nle nouveau mot de passe")
     }
     /**
      Action that modifies user name
      */
-    @objc private func handleModifyUserName(){
+    @objc func handleModifyUserName(){
         print("here modify user name")
         alertToModifyName(title: "Modification du nom d'utilisateur", message: "Indiquer le mot de passe et\nindiquer votre nouveau nom utilisateur")
     }
     /**
      Action that modifies user name
      */
-    @objc private func handleModifyEmail(){
+    @objc func handleModifyEmail(){
         print("here modify user email")
         alertToModifyEmail(title: "Modification de l'email", message: "Indiquer le mot de passe et\nindiquer votre nouvel email")
     }
-    
     func launchPicker() {
         // create a instance of UIImagePickerController()
         let picker = UIImagePickerController()
@@ -310,7 +146,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
         }
         uploadTask.resume()
     }
-    
     @objc func alertIdenticalPassword() {
         Alert.shared.controller = self
         Alert.shared.alertDisplay = .invalidPasswordChangeIdentical
@@ -360,7 +195,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
         alert.addAction(UIAlertAction(title: "Annuler", style: .default) { (alertAction) in })
         self.present(alert, animated:true, completion: nil)
     }
-
     /**
         Function that creates an alert with 2 textfields to modify password
         - Parameter title: The alert title
@@ -398,7 +232,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
                 }
             }
         })
-        
         //Step : 3
         //For first TF
         alert.addTextField { (textField) in
@@ -457,16 +290,11 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
         alertC.addAction(UIAlertAction(title: "Annuler", style: .default) { (alertAction) in })
         self.present(alertC, animated:true, completion: nil)
     }
-    
-    
     func isValidEmail(testStr:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
     }
-    
-    
-    
     func changePassword(email: String, currentPassword: String, newPassword: String, completion: @escaping Completion) {
         if let currentUser = Auth.auth().currentUser {
             let credential = EmailAuthProvider.credential(withEmail: email, password: currentPassword)
@@ -483,7 +311,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
             })
         }
     }
-    
     func changeEmail(email: String, currentPassword: String, newEmail: String, completion: @escaping Completion) {
         if let currentUser = Auth.auth().currentUser {
             let credential = EmailAuthProvider.credential(withEmail: email, password: currentPassword)
@@ -499,7 +326,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
             })
         }   
     }
-    
     func changeName(email: String, currentPassword: String, newName: String) {
             guard  let uid = Auth.auth().currentUser?.uid else {return}
             let credential = EmailAuthProvider.credential(withEmail: email, password: currentPassword)
@@ -515,7 +341,6 @@ class SettingsViewController: UIViewController, GADRewardedAdDelegate, GADReward
             })
         }
 }
-
 extension SettingsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func  imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         var selectedImageFromPicker = UIImage()
